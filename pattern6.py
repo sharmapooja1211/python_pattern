a97d98c8fb7bb087fa0b8df9af5fb5ed3b57d287
Lenovo <sharmapooja1211@gmail.com>
2025-05-07 14:27:52 +0530

    commit pattern

---
+++ pattern6.py
@@ -0,0 +1,14 @@
+"""
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+"""
+
+rows = 5
+for i in range(rows):
+    spaces = '  ' * (rows - i - 1)
+    stars = '* ' * (2 * i + 1)
+    print(spaces + stars)
